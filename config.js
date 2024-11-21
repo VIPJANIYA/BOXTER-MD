9d538bca8046a216bd6e6286429ebc8030373fc0
VIPJANIYA <rmkarunarathna052@gmail.com>
2024-11-21 23:05:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "HVsWVRRY#8csrcGH7W9FezlerwhReLaT7CsxqQjwYHbp2BJWFf2w",
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 };
